7559dead7d0c0f050b1106697e2cf7e84f912843
KyungHyunLim <72729802+KyungHyunLim@users.noreply.github.com>
2021-03-23 21:06:21 +0900

    Update 1861.cpp

--- SWEA/1861.cpp
+++ SWEA/1861.cpp
@@ -2,19 +2,18 @@
 정사각형의 방
 
 핵심: 
-1. 한방에서 갈수 있는 곳은 1개 => 같은 방을 두번 방문할 필요는 없다.
-2. 갈수있는 방이 +1이므로 4번의 이동가능 수 = 3번의 이동가능수 + 1 이다.
-
-BFS로 풀 수 있을까? 시간 초과
-메모라이제이션 필요
+1. 한방에서 갈수 있는 곳은 1개 => 같은 방을 두번 방문할 필요는 없다. (dfs)
+2. 갈수있는 방이 +1이므로 4번의 이동가능 수 = 3번의 이동가능수 + 1 이다. 
 */
 #include<iostream>
 #include<vector>
 #include <cstring>
-#include<queue>
+
 using namespace std;
 
 #define INF 987654321
+#define MAX(A,B) A>B?A:B
+#define MIN(A,B) A<B?A:B
 
 int N;
 int map[1000][1000];
@@ -38,41 +37,19 @@ struct info {
 	info(int _x, int _y, int _cnt, int _start) : x(_x), y(_y), cnt(_cnt), start(_start){};
 };
 
-void solve(int i, int j) {
-	queue<info> q;
-	q.push(info(i,j,1, map[i][j]));
-	visited[i][j] = 1;
-
-	while (!q.empty()) {
-		int x = q.front().x;
-		int y = q.front().y;
-		int c = q.front().cnt;
-		int start = q.front().start;
-		q.pop();
-		
-		if (c > cnt) {
-			ans = start;
-			cnt = c;
-		}
-		else if(c==cnt) {
-			if (ans > start) {
-				ans = start;
-			}
-		}
-
-		for (int k = 0; k < 4; k++) {
-			int nx = x + dx[k];
-			int ny = y + dy[k];
-			if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
-			if (map[x][y] + 1 == map[nx][ny]) {
-				if(visited[nx][ny] != 0){
-					visited[nx][ny] = visited[x][y] + 1;
-				}else{
-					q.push(info(nx, ny, c + 1, start));
-				}
-			}
-		}
-	}
+void solve(int x, int y) {
+	visited[x][y] = 1;
+    
+    for(int i=0; i<4; i++){
+     	int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+        if(map[nx][ny] == 0) continue;
+        if(map[nx][ny] == map[x][y] + 1){
+         	if(visited[nx][ny] == 0) solve(nx,ny);
+            visited[x][y] = MAX(visited[x][y], visited[nx][ny] + 1);
+        }       
+    }
 }
 
 int main(int argc, char** argv)
@@ -96,15 +73,27 @@ int main(int argc, char** argv)
 			}
 		}
 	
-		ans = INF;
-		cnt = 0;
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				if(!visited[i][j]) solve(i, j);
 			}
 		}
-		
-
+        
+		ans = INF;
+		cnt = 0;
+        
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (cnt < visited[i][j]) {
+				    cnt = visited[i][j];
+				    ans = map[i][j];
+				}
+				else if (cnt == visited[i][j]) {
+					ans = MIN(ans, map[i][j]);
+				}
+			}
+		}
+        
 		cout << "#" << test_case << " " << ans  << " " << cnt << endl;
 	}
 
