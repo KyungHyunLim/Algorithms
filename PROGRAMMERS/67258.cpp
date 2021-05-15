2903ad3dd2e787b8e349f28d0878588a31ecb974
KyungHyunLim <72729802+KyungHyunLim@users.noreply.github.com>
2021-05-15 11:01:50 +0900

    Update 67258.cpp

--- PROGRAMMERS/67258.cpp
+++ PROGRAMMERS/67258.cpp
@@ -76,3 +76,77 @@ vector<int> solution(vector<string> gems) {
     // 맨첫번째 원소 반환
     return {candidates[0].first, candidates[0].second};
 }
+
+//--------------------------------------------------------------------------------------------------------------
+/*
+후보를 저장하지 않고, 투포인터 인덱스를 활용해 바로바로 업데이트
+2-3 과정 삭제
+
+1. 각 보석의 좌표 없데이트
+
+결과: 
+정확성: 100%, 효율성 100%
+*/
+//--------------------------------------------------------------------------------------------------------------
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<string> gems) {
+    int min_len = 987654321;
+    // 보석 종류 개수 구하기
+    set<string> gs;
+    for(int i=0; i<gems.size(); i++){
+        gs.insert(gems[i]);
+    }
+    // 보석 종류 개수
+    int MAX_GEM = gs.size();
+    
+    map<string, int> info;
+    // 현재 구입한 보석의 종류 개수
+    int count = 0;
+    // 후보 시작좌표 및 끝좌표
+    int cur_s, cur_e;
+    int s_idx = 0, e_idx = 0;
+    while(true){
+        // 보석의 종류를 전부 구입했으면
+        if(count == MAX_GEM ){
+            // 구간 길이가 더 작으면 업데이트
+            if(e_idx - s_idx < min_len){
+                cur_s = s_idx;
+                cur_e = e_idx;
+                min_len = e_idx - s_idx;
+            }
+            // 다음 구간으로 슬라이딩
+            // 시작 좌표의 보석의 개수가 1보다 많으면
+            if(info[gems[s_idx]] > 1){
+                // 개수 줄이고, 좌표만 업데이트
+                info[gems[s_idx]]--;
+                s_idx++;
+            }
+            else{
+                // 1개면, 개수 줄이고, 좌표 업데이트 후
+                // 구입 종류 개수 감소
+                info[gems[s_idx]]--;
+                s_idx++;
+                count--;
+            }
+        }
+        // 보석 종류를 모두 구입하지 못한 경우
+        else{
+            // 끝좌표 도달시 탐색 종료
+            if(e_idx == gems.size()) break;
+            // 해당 보석이 0개였으면 보석 종류 증가
+            if(!info[gems[e_idx]]) count ++;
+            // 보석 개수 증가
+            info[gems[e_idx]]++;
+            // 끝좌표 증가
+            e_idx++;
+        }
+    }
+    
+    return {cur_s + 1, cur_e};
+}
